41ae99ca07745f8888b870edff9ce90ecb430e79
thaislino2@gmail.com <thaislino2@gmail.com>
2021-04-21 18:59:56 -0300

    criado o modelo de pessoas

--- beer_review/home/models.py
+++ beer_review/home/models.py
@@ -9,6 +9,7 @@ class Cervejas(models.Model):
     description = models.CharField(max_length=300)
     Tipo = models.CharField(max_length=150, default="", editable=True)
     image = models.ImageField(blank=True, upload_to='home/images')
+    nota = models.IntegerField(default="5", max_length=5)
 
     def __str__(self):
         return self.title
