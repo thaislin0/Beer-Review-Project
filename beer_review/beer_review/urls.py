4bf996e8e62cb47e1654214a7da76466fbf00d5e
thaislino2@gmail.com <thaislino2@gmail.com>
2021-04-21 14:29:03 -0300

    starting to do the cerveja page

--- beer_review/beer_review/urls.py
+++ beer_review/beer_review/urls.py
@@ -8,7 +8,7 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('review', views.review, name='review'),
     path('catalogo', views.catalogo, name='catalogo'),
     path('cerveja', views.cerveja, name='cerveja'),
--- beer_review/home/urls.py
+++ beer_review/home/urls.py
@@ -5,7 +5,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('review', views.review, name='review'),
     path('catalogo', views.catalogo, name='catalogo'),
     path('cerveja', views.cerveja, name='cerveja')
