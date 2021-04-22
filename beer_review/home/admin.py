e180b340336ebe05bfd0ec257a3722eef588ed87
thaislino2@gmail.com <thaislino2@gmail.com>
2021-04-21 21:52:15 -0300

    incluido a secao de buscar

--- beer_review/home/admin.py
+++ beer_review/home/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Cervejas
 
-# Register your models here.
 
-admin.site.register(Cervejas)
+class ListandoCervejas(admin.ModelAdmin):
+    list_display = ('id', 'title', 'Origem', 'Tipo', 'Alcool', 'Familia')
+    list_display_links = ('id', 'title',)
+    search_fields = ('id', 'title', 'Origem', 'Tipo', 'Alcool', 'Familia')
+    list_filter = ('id', 'title', 'Origem', 'Tipo', 'Alcool', 'Familia')
+    list_per_page = 10
+
+
+admin.site.register(Cervejas, ListandoCervejas)
--- beer_review/home/models.py
+++ beer_review/home/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from pessoas.models import Pessoa
 
 
 class Cervejas(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     Origem = models.CharField(max_length=150)
     Alcool = models.IntegerField()
@@ -9,7 +11,7 @@ class Cervejas(models.Model):
     description = models.CharField(max_length=300)
     Tipo = models.CharField(max_length=150, default="", editable=True)
     image = models.ImageField(blank=True, upload_to='home/images')
-    nota = models.IntegerField(default="5", max_length=5)
+    nota = models.IntegerField(default=5)
 
     def __str__(self):
         return self.title
