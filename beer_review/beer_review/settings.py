91b430bb331e653bfbad701c0469a7d31840ab55
thaislino2@gmail.com <thaislino2@gmail.com>
2021-04-27 10:28:17 -0300

    fixing static

--- beer_review/beer_review/settings.py
+++ beer_review/beer_review/settings.py
@@ -120,11 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static'
 STATIC_ROOT = '/static/'
-STATICFILES_DIRS = [
-    ('home/static')
-]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
